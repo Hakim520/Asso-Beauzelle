50cb7ed0f9e570168a4bb91a2beb5ab01af28d75
Hakim <95052573+Hakim520@users.noreply.github.com>
2026-02-26 14:42:47 +0100

    modifying aboutMe section

--- App/frontend/src/components/About.jsx
+++ App/frontend/src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
       <div className="container">
         <div className="row align-items-center gy-4">
 
-          <div className="col-lg-6">
+          <div className="col-lg-5">
             <img
               src="/about_me.png"
               alt="About"
@@ -13,7 +13,7 @@ function About() {
           </div>
 
           <div className="col-lg-6">
-            <h2 className="fw-bold mb-3">Lorem Ipsum Dolor</h2>
+            <h2 className="fw-bold mb-3">Qui sommes-nous ?</h2>
             <p className="text-muted">
              
           L'Association Fraternité de Beauzelle est une association loi 1901 engagée au service du vivre-ensemble. Nous menons des actions sociales, solidaires et éducatives au profit de la population locale, et assurons la gestion d'un lieu de culte ouvert à tous dans un esprit de respect, de partage et de fraternité.
